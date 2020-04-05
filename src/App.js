33e10f5fdff486a500bd2549f2cd4343a6999a93
alid didarali <aliddidarali02@gmail.com>
2020-04-05 12:03:15 +0000

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import AssignList from "./pages/AssignmentList";
+import { PageHeader } from 'antd';
+import AssignmentList from "./pages/AssignmentList";
 import "./App.css";
 
+// const arr = [1, 2, 3]
 function App() {
-  return (
-    <div className="App">
-      <AssignList />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PageHeader
+                className="site-page-header"
+                ghost={false}
+                title="Loin Heart Acedemy"
+            />
+            <AssignmentList />
+        </div>
+    );
 }
 
 export default App;
--- src/pages/AssignmentList.js
+++ src/pages/AssignmentList.js
@@ -1,52 +1,52 @@
 import React from "react";
-import "./assignmentlist.css";
-import "antd/dist/antd.css";
-import { Card, PageHeader } from "antd";
+import { Card, List } from "antd";
 import { getAllAssignments } from "api";
+import "./AssignmentList.css";
 
 class AssignList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
-  async componentDidMount() {
-    const dataOnMount = await getAllAssignments();
-    this.setState({ data: dataOnMount.items });
-  }
-
-  render() {
-    if (this.state.data.length > 0) {
-      return (
-        <div>
-          <PageHeader
-            className="site-page-header"
-            ghost={false}
-            title="Loin Heart Acedemy"
-          />
-          <div className="site-card-border-less-wrapper">
-            <h2>Assignment list</h2>
-            <div className="Card-list-wrapper">
-              {this.state.data.map(res => (
-                <div className="card-border-wrapper" key={res.id}>
-                  <Card hoverable className="assignment">
-                    <h3>{res.title}</h3>
-                    <div className="date-wrapper">
-                      <h4>Start On : {res.setOn}</h4>
-                      <h4>Complete On : {res.deadline}</h4>
-                    </div>
-                  </Card>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return null;
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
     }
-  }
+    async componentDidMount() {
+        const dataOnMount = await getAllAssignments();
+        this.setState({ data: dataOnMount.items });
+    }
+
+    render() {
+        if (this.state.data.length > 0) {
+            return (
+                <div>
+                    <h2>Assignment list</h2>
+                    <List style={{ 'padding': '10px' }}
+                        grid={{
+                            gutter: 10,
+                            xs: 1,
+                            sm: 1,
+                            md: 2,
+                            lg: 3,
+                            xl: 3,
+                            xxl: 3,
+                        }}
+                        dataSource={this.state.data}
+                        renderItem={item => (
+                            <List.Item>
+                                <Card className='assignment'>
+                                    <div className="date-wrapper">
+                                        <h4>Start On : {item.setOn}</h4>
+                                        <h4>Complete On : {item.deadline}</h4>
+                                    </div></Card>
+                            </List.Item>
+                        )}
+                    />,
+                </div >
+            );
+        } else {
+            return null;
+        }
+    }
 }
 
 export default AssignList;
