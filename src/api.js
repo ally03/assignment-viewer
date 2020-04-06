cfdc3a8242c13b8feaeea622a6096de4d18c0ff8
alid didarali <aliddidarali02@gmail.com>
2020-04-06 07:43:16 +0000

    showing errors corerctly.

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 export async function getAllAssignments() {
-  return await fetch(
-    "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/test/assignments?pageIndex=0&pageSize=30"
-  );
+    return await fetch(
+        // 'https://httpstat.us/404'
+        "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/test/assignments?pageIndex=0&pageSize=30"
+    );
 }
 
 export async function getAllAssignmentsDetails(id) {
-  return await fetch(
-    // "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/195212313"
-    `https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/${id}`
-  );
+    return await fetch(
+        // "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/195212313"
+        `https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/${id}`
+    );
 }
