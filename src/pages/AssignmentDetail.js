e2df6f521245a4db364cd7b273cc5968a98405e1
alid didarali <aliddidarali02@gmail.com>
2020-04-09 11:35:07 +0100

    added complete button functionality

--- src/pages/AssignmentDetail.js
+++ src/pages/AssignmentDetail.js
@@ -2,77 +2,75 @@ import React from "react";
 import { Modal, Button, Alert } from "antd";
 
 class AssignmentDetail extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: props.status,
-            complete: {}
-        };
-        this.handleComplete = this.handleComplete.bind(this);
+  assignDetails() {
+    if (this.props.assignmentDetailError !== undefined) {
+      return (
+        <div className="errorBox">
+          <Alert
+            message={this.props.assignmentDetailError.errorCode}
+            description={this.props.assignmentDetailError.errorMsg}
+            type="error"
+            showIcon
+          />
+        </div>
+      );
+    } else {
+      const removePtag = this.props.assignmentDetail.details.replace(
+        /<[^>]+>/g,
+        ""
+      );
+      return (
+        <div>
+          <p>Assignment Detail: {removePtag}</p>
+          <div>
+            <p>Start On : {this.props.assignmentDetail.setOn}</p>
+            <p>Complete On : {this.props.assignmentDetail.deadline}</p>
+          </div>
+          <div>
+            <h4>Given By</h4>
+            <p>
+              First Name : {this.props.assignmentDetail.setBy.title}{" "}
+              {this.props.assignmentDetail.setBy.firstName}
+            </p>
+            <p>Last Name : {this.props.assignmentDetail.setBy.lastName}</p>
+            <p>Last Name : {this.props.assignmentDetail.setBy.email}</p>
+            <p>
+              School Name: {this.props.assignmentDetail.groups[0].school.name}
+            </p>
+          </div>
+        </div>
+      );
     }
-    handleComplete() {
-        this.setState({
-            complete: {
-                assignComplete: true,
-                completeId: this.props.assignmentDetail.id
-            }
-        });
-    }
-    assignComplete() {
-           return this.state.complete.assignComplete ? "Completed" : "Incomplete"; 
-    }
-    assignDetails() {
-        if (this.props.assignmentDetailError !== undefined) {
-            return (
-                <div className='errorBox'>
-                    <Alert
-                        message={this.props.assignmentDetailError.errorCode}
-                        description={this.props.assignmentDetailError.errorMsg}
-                        type="error"
-                        showIcon
-                    />
-                </div>
-            )
-        } else {
-            const removePtag = this.props.assignmentDetail.details.replace(/<[^>]+>/g, "");
-            return (
-                <div>
-                    <p>Assignment Detail: {removePtag}</p>
-                    <div>
-                        <p>Start On : {this.props.assignmentDetail.setOn}</p>
-                        <p>Complete On : {this.props.assignmentDetail.deadline}</p>
-                    </div>
-                    <div>
-                        <h4>Given By</h4>
-                        <p>
-                            First Name : {this.props.assignmentDetail.setBy.title}{" "}
-                            {this.props.assignmentDetail.setBy.firstName}
-                        </p>
-                        <p>Last Name : {this.props.assignmentDetail.setBy.lastName}</p>
-                        <p>Last Name : {this.props.assignmentDetail.setBy.email}</p>
-                        <p>School Name: {this.props.assignmentDetail.groups[0].school.name}</p>
-                    </div>
-                </div>
-            )
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Modal
-                    title={this.props.assignmentDetail && this.props.assignmentDetail.title}
-                    visible={this.state.visible}
-                    footer={[
-                        <Button key="submit" type="primary" onClick={this.handleComplete}>{this.assignComplete()}
-                        </Button>,
-                        <Button key="Close" onClick={this.props.hideModal}>Close</Button>
-                    ]}
-                >
-                    {this.assignDetails()}
-                </Modal>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div>
+        <Modal
+          title={
+            this.props.assignmentDetail && this.props.assignmentDetail.title
+          }
+          closable={false}
+          visible={this.props.status}
+          footer={[
+            <Button
+              key="submit"
+              type={this.props.isComplete ? "primary" : "danger"}
+              onClick={() =>
+                this.props.completeAction(this.props.assignmentDetail.id)
+              }
+            >
+              {this.props.isComplete ? "Complete" : "Incomplete"}
+            </Button>,
+            <Button key="Close" onClick={this.props.hideModal}>
+              Close
+            </Button>
+          ]}
+        >
+          {this.assignDetails()}
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default AssignmentDetail;
