109737828e7fba21d097a22f8eacbe42eb7e4888
alid didarali <aliddidarali02@gmail.com>
2020-04-11 13:15:39 +0000

    removed unnecessary async await

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
-export async function getAllAssignments() {
-    return await fetch(
+export function getAllAssignments() {
+    return fetch(
         // 'https://httpstat.us/404'
         "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/test/assignments?pageIndex=0&pageSize=30"
     );
 }
 
-export async function getAllAssignmentsDetails(id) {
-    return await fetch(
+export function getAllAssignmentsDetails(id) {
+    return fetch(
         // "https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/195212313"
         `https://beehiveapi.lionhearttrust.org.uk/v3.5/planner/users/d70cbe8d-11ac-e811-80e2-005056a23846/assignments/${id}`
     );
